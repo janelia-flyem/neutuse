e47b160c1fec55f9c35adfd00cbb8a8a4c7fdfc1
deli <ephemera_2015@sina.com>
2018-08-28 07:45:31 +0800

    add client filter

--- tests/post_skeletonize.py
+++ tests/post_skeletonize.py
@@ -6,7 +6,6 @@ SERVER='http://127.0.0.1:5000/api/v1/tasks/'
 HEADERS={'Content-Type':'application/json'}
 
 config={
-    'start_cmd':'neutu_d',
     'output':'/home/deli/result.swc',
     'input':'/home/deli/t.tif',
     'force_update':True
@@ -14,13 +13,17 @@ config={
 }
 
 rq.post(SERVER,data=json.dumps({'type':'dvid','name':'skeletonize','config':config}),headers=HEADERS)
+rq.post(SERVER,data=json.dumps({'type':'dvid','name':'skeletonize','config':config}),headers=HEADERS)
+rq.post(SERVER,data=json.dumps({'type':'dvid','name':'skeletonize','config':config}),headers=HEADERS)
+
 
 config={
-    'start_cmd':'neutu_d',
     'bodyid':1,
     'input':'address:port:uuid:labelvol',
     'force_update':True
     
 }
 
+rq.post(SERVER,data=json.dumps({'type':'dvid','name':'skeletonize','config':config}),headers=HEADERS)
+rq.post(SERVER,data=json.dumps({'type':'dvid','name':'skeletonize','config':config}),headers=HEADERS)
 rq.post(SERVER,data=json.dumps({'type':'dvid','name':'skeletonize','config':config}),headers=HEADERS)
