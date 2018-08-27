c2aa6d417acd83c608106e6b3feb4bde3f6ad442
deli <ephemera_2015@sina.com>
2018-08-27 23:02:09 +0800

    use argparse

--- neutuse/app.py
+++ neutuse/app.py
@@ -1,27 +1,40 @@
 import os
 import sys
+import argparse
 
 from flask import Flask
 
 from api import task_manager_http_api_v1
 import webclient
-from service import run_service
+from service import Skeletonize
 
-def run_app(host,port):
+
+parser = argparse.ArgumentParser()
+parser.add_argument('action',type=str, choices=['run', 'post'])
+parser.add_argument('-t', '--type', type=str, choices=['taskmanager','service'])
+parser.add_argument('-a', '--addr', type=str, default='127.0.0.1')
+parser.add_argument('-p', '--port', type=int, default=5000)
+parser.add_argument('-n', '--name', type=str, default='skeletonize')
+args=parser.parse_args()
+
+
+def run_taskmanager():
     app = Flask(__name__)
-    webclient.init(host, port)
+    webclient.init(args.addr, args.port)
     app.register_blueprint(task_manager_http_api_v1.bp, url_prefix='/api/v1/tasks')
     app.register_blueprint(webclient.bp, url_prefix='/client')
-    app.run(host=host, port=port)
+    app.run(host=args.addr, port=args.port)
 
-if __name__ == '__main__':    
-    mode = sys.argv[1]
-    if mode == 'taskmanager':
-        host,port = sys.argv[2].split(':')
-        port = int(port)
-        run_app(host, port)
-    elif mode == 'service':
-        name = sys.argv[2]
-        host,port = sys.argv[3].split(':')
-        port = int(port)
-        run_service(name, host, port)
+def run_service():
+    if args.name == 'skeletonize':
+        Skeletonize(args.addr, args.port).run()
+    else:
+        print(name + ' service has not been supported yet')
+    
+if args.action == 'run':
+    if args.type == 'taskmanager':
+        run_taskmanager()
+    elif args.type == 'service':
+        run_service()
+elif args.action == 'post':
+    pass
--- neutuse/service/__init__.py
+++ neutuse/service/__init__.py
@@ -1,8 +1,4 @@
 from .skeletonize import Skeletonize 
 
-def run_service(name, host, port):
-    if name == 'skeletonize':
-        Skeletonize(host,port).run()
-    else:
-        print(name + ' service has not been supported yet')
+
     
