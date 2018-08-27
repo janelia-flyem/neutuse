3611321ccad7ef452817af723aee85d6362dce92
deli <ephemera_2015@sina.com>
2018-08-28 07:40:28 +0800

    add client filter

--- neutuse/neutuse.py
+++ neutuse/neutuse.py
@@ -11,7 +11,7 @@ parser.add_argument('action',type=str, choices=['run'])
 parser.add_argument('-t', '--type', type=str, choices=['taskmanager','service'])
 parser.add_argument('-a', '--addr', type=str, default='127.0.0.1:5000')
 parser.add_argument('-n', '--name', type=str, default='skeletonize')
-parser.add_argument('-d', '--debug', action='store_true', default=True)
+parser.add_argument('-d', '--debug', action='store_true', default=False)
 args=parser.parse_args()
 
 
--- neutuse/run.sh
+++ neutuse/run.sh
@@ -2,62 +2,6 @@
 
 NEUTUSE=${HOME}/.local/share/neutuse
 
-neutuse_help(){
-    echo 'Usage:'
-    echo 'For help:'
-    echo 'neutuse help'
-    echo 'Run taskmanager:'
-    echo 'neutuse run taskmanager [addr]'
-    echo "Default addr is 127.0.0.1:5000"
-    echo 'Run service:'
-    echo 'neutuse run service {service_name}[taskmanager addr]'
-}
-
-
-neutuse_run_taskmanager(){
-    cd ${NEUTUSE}
-    source activate neutuse
-    addr="127.0.0.1:5000"
-    if [ $# -ge 1 ]; then
-        addr=$1
-    fi
-    python app.py taskmanager ${addr}  
-}
-
-neutuse_run_service(){
-    cd ${NEUTUSE}
-    source activate neutuse
-    if [ $# -lt 1 ]; then
-        echo 'please specify service name'
-        exit 1
-    fi
-    name=$1
-    if [ $# -ge 2 ]; then
-        addr=$2
-    else
-        addr="127.0.0.1:5000"
-    fi
-    python app.py "service" ${name} ${addr}
-}
-
-neutuse_run(){
-    case $1 in 
-    taskmanager)
-        shift 
-        neutuse_run_taskmanager $*;;
-    service)
-        shift 
-        neutuse_run_service $*;;
-    *) neutuse_help;;
-    esac
-}
-
-neutuse_main(){
-    case $1 in
-    help) neutuse_help ;;
-    run)  shift
-          neutuse_run $*;;
-    *) neutuse_help;;
-    esac
-}
-neutuse_main $*
+source activate neutuse
+cd ${NEUTUSE}
+python neutuse.py $*
