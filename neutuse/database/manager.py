3b30564bf28df570280bcec649a2375672308680
Deli Wang <ephemera_2015@sina.com>
2018-10-26 09:43:11 +0800

    return existing task's id when task already exists

--- neutuse/database/manager.py
+++ neutuse/database/manager.py
@@ -198,9 +198,8 @@ class TaskManager(SubManager):
         if 'config' in task:
             config = task['config']
             for t in self.query({'type': task['type'], 'name': task['name']}):
-                print(1)
                 if t['config'] == config and t['status'] == 'submitted':
-                    raise(Exception('Task already exists'))
+                    return t['id']
         return super(TaskManager, self).add(task)
                   
     @session_wrapper    
